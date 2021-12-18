a73057b114f9fdeced0d6b2fa0124fd8fbd1072e
AghansCode <ganis.ade23@gmail.com>
2021-12-18 18:40:26 +0700

    memperbaiki cors origin access control

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(multer({ storage: fileStorage, fileFilter, fileStorage }).single("image"
 //HANDLE CORS ORIGIN
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
